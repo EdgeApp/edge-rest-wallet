761c6f3654ef44e93e1830781b1573eb60dc5edf
mulderkristen <mulderkristen@gmail.com>
2020-02-24 16:22:04 -0800

    WIP basic express functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+
 import CONFIG from '../config.json'
 
-function foo(): void {
-  console.log('Hello, world')
-  console.log('apiKey:', CONFIG.apiKey)
-}
+const app = express()
 
-foo()
+const persons = [
+  {
+    name: 'Jon George',
+    age: 45
+  }
+]
+
+app.listen(CONFIG.httpPort, () => {
+  console.log('Server is listening on:', CONFIG.httpPort)
+})
+
+app.use(bodyParser.json({ limit: '1mb' }))
+
+app.get('/persons', (req, res, next) => {
+  res.send(persons)
+})
+
+app.post('/persons', (req, res, next) => {
+  const receivedPerson = req.body
+  if (
+    receivedPerson.name === CONFIG.name &&
+    receivedPerson.age === CONFIG.age
+  ) {
+    persons.push(receivedPerson)
+    res.json(persons)
+  } else {
+    res.json(false)
+  }
+})
