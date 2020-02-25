b12c47c11086298914318c9c9b67aaea842a28e1
mulderkristen <mulderkristen@gmail.com>
2020-02-25 10:46:43 -0800

    Added get balances request which sends a response of the balance by wallet type

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
 import bodyParser from 'body-parser'
+import {
+  addEdgeCorePlugins,
+  EdgeAccount,
+  EdgeContext,
+  EdgeCurrencyWallet,
+  lockEdgeCorePlugins,
+  makeEdgeContext
+} from 'edge-core-js'
+import bitcoinPlugins from 'edge-currency-bitcoin'
 import express from 'express'
 
 import CONFIG from '../config.json'
+addEdgeCorePlugins(bitcoinPlugins)
+lockEdgeCorePlugins()
 
-const app = express()
+async function main(): Promise<void> {
+  const app = express()
 
-const persons = [
-  {
-    name: 'Jon George',
-    age: 45
-  }
-]
+  // Start the core, with Bitcoin enabled:
+  const context: EdgeContext = await makeEdgeContext({
+    apiKey: CONFIG.apiKey,
+    appId: CONFIG.appId,
+    plugins: CONFIG.plugins
+  })
 
-app.listen(CONFIG.httpPort, () => {
-  console.log('Server is listening on:', CONFIG.httpPort)
-})
+  // Log in to some user:
+  const account: EdgeAccount = await context.loginWithPassword(
+    CONFIG.username,
+    CONFIG.password
+  )
 
-app.use(bodyParser.json({ limit: '1mb' }))
+  app.use(bodyParser.json({ limit: '1mb' }))
 
-app.get('/persons', (req, res, next) => {
-  res.send(persons)
-})
+  // Getting wallet balances based on type of wallet
+  app.get('/balances/', async (req, res, next) => {
+    const type = req.query.type
+    const walletInfo = account.getFirstWalletInfo(`wallet:${type}`)
+    if (walletInfo == null) {
+      res.status(404).send(`${type} is invalid`)
+      return
+    }
+    try {
+      const wallet: EdgeCurrencyWallet = await account.waitForCurrencyWallet(
+        walletInfo.id
+      )
+      res.send(wallet.balances)
+    } catch (e) {
+      res.status(500).send('Server error in waitForCurrencyWallet')
+    }
+  })
 
-app.post('/persons', (req, res, next) => {
-  const receivedPerson = req.body
-  if (
-    receivedPerson.name === CONFIG.name &&
-    receivedPerson.age === CONFIG.age
-  ) {
-    persons.push(receivedPerson)
-    res.json(persons)
-  } else {
-    res.json(false)
-  }
+  app.listen(CONFIG.httpPort, () => {
+    console.log('Server is listening on:', CONFIG.httpPort)
+  })
+}
+main().catch(e => {
+  console.error(e)
+  process.exit(1)
 })
